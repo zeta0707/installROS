37ecc30bfdaa19906dd026b04ccdc447f01d3fb6
Changwhan Lee <zeta0707@gmail.com>
2021-07-04 19:48:56 +0900

    Update install-ros.sh
    . wrong pgp key -> use curl
    . add clean before build

--- install-ros.sh
+++ install-ros.sh
@@ -7,7 +7,9 @@ echo "[Add the ROS repository]"
 if [ ! -e /etc/apt/sources.list.d/ros-latest.list ]; then
     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 fi
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 echo "[Update the package]"
 sudo apt-get update
@@ -30,6 +32,7 @@ mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws
 source /opt/ros/melodic/setup.bash
 catkin init
+catkin clean -y
 catkin build
 sh -c "echo \"source ~/catkin_ws/devel/setup.bash\" >> ~/.bashrc"
 source ~/.bashrc
